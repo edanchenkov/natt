5f70ad335b80784b012a782b961c40f8b3da89fe
Egor Danchenkov <icepnyx@gmail.com>
2017-04-19 09:56:14 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
+/* -----------------------------------------------------
+    Index.ts
+
+    Code below is taken from:
+      https://www.typescriptlang.org/docs/handbook/classes.html
+    Remove at will.
+
+    Start writing your code in this file.
+    Happy coding, have fun!
+-------------------------------------------------------- */
 class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
+  private greeting: string;
+
+  /**
+   * Greeter constructor
+   * @param  {string} message [description]
+   */
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  /**
+   * Generate greeeting string text
+   * @return {string} Return full greeting text
+   */
+  public greet(): string {
+    return "Hello, " + this.greeting;
+  }
 }
 
 let greeter = new Greeter("world");
-
-let button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function() {
-    alert(greeter.greet());
+if (typeof document !== "undefined") {
+  document.body.innerHTML = `<div>${greeter.greet()}</div>`;
 }
-
-document.body.appendChild(button);
